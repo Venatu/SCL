0973f192291bc9584b1ba27bd771b22bb5f67e49
Venatu <Michael@LIDDIARD.local>
2016-09-19 23:22:26 +0100

    Initial revit add-in
    Created the initial revit add-in code with an example of pulling
    structural elements and some of there material properties

---
+++ RevitLink/Main.cs
@@ -0,0 +1,55 @@
+﻿using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Venatu.SCL.RevitLink
+{
+    class Main : IExternalApplication
+    {
+        public static string AssemblyDirectory
+        {
+            get
+            {                
+                return Assembly.GetExecutingAssembly().Location;
+                /*string codeBase = Assembly.GetExecutingAssembly().CodeBase;
+                UriBuilder uri = new UriBuilder(codeBase);
+                string path = Uri.UnescapeDataString(uri.Path);
+                return Path.GetDirectoryName(path + "RevitLink.dll");*/
+            }
+        }
+
+        public Result OnShutdown(UIControlledApplication application)
+        {
+            return Result.Succeeded;
+        }
+
+        public Result OnStartup(UIControlledApplication application)
+        {
+            // Create a custom ribbon tab
+            String tabName = "Structure Compute Library";
+            application.CreateRibbonTab(tabName);
+
+            // Create two push buttons
+            PushButtonData button1 = new PushButtonData("SyncButton", "Sync Model",
+                AssemblyDirectory, "Venatu.SCL.RevitLink.SyncModel");
+            PushButtonData button2 = new PushButtonData("Button2", "My Button #2",
+                AssemblyDirectory, "Venatu.SCL.RevitLink.SyncModel");
+
+            // Create a ribbon panel
+            RibbonPanel m_projectPanel = application.CreateRibbonPanel(tabName, "This Panel Name");
+
+            // Add the buttons to the panel
+            List<RibbonItem> projectButtons = new List<RibbonItem>();
+            projectButtons.AddRange(m_projectPanel.AddStackedItems(button1, button2));
+
+            return Result.Succeeded;
+        }
+
+        
+    }
+}
---
+++ RevitLink/SyncModel.cs
@@ -0,0 +1,91 @@
+﻿using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Structure;
+using Autodesk.Revit.Attributes;
+
+namespace Venatu.SCL.RevitLink
+{
+    [TransactionAttribute(TransactionMode.Automatic)]
+    class SyncModel : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            //Get the current document
+            Document doc = commandData.Application.ActiveUIDocument.Document;
+
+            BuiltInCategory[] bics = new BuiltInCategory[] {
+    BuiltInCategory.OST_StructuralColumns,
+    BuiltInCategory.OST_StructuralFraming,
+    BuiltInCategory.OST_StructuralFoundation
+            };
+
+            IList<ElementFilter> a = new List<ElementFilter>(bics.Count());
+
+            foreach (BuiltInCategory bic in bics)
+            {
+                a.Add(new ElementCategoryFilter(bic));
+            }
+
+            LogicalOrFilter categoryFilter = new LogicalOrFilter(a);
+
+            LogicalAndFilter familyInstanceFilter = new LogicalAndFilter(categoryFilter, new ElementClassFilter(typeof(FamilyInstance)));
+
+            IList<ElementFilter> b = new List<ElementFilter>(6);
+
+            /*b.Add(new ElementClassFilter(
+              typeof(Wall)));
+
+            b.Add(new ElementClassFilter(
+              typeof(Floor)));
+
+            b.Add(new ElementClassFilter(
+              typeof(ContFooting)));
+
+            b.Add(new ElementClassFilter(
+              typeof(PointLoad)));
+
+            b.Add(new ElementClassFilter(
+              typeof(LineLoad)));
+
+            b.Add(new ElementClassFilter(
+              typeof(AreaLoad)));*/
+
+            b.Add(familyInstanceFilter);
+
+            LogicalOrFilter classFilter = new LogicalOrFilter(b);
+
+            FilteredElementCollector collector = new FilteredElementCollector(doc);
+            collector.WherePasses(classFilter);
+            var filteredElements = collector.ToElements();
+
+            
+
+            foreach(Element e in filteredElements)
+            {
+                /*
+                var analytical = e.GetAnalyticalModel();                
+                var materials = e.GetMaterialIds(false);
+                var mat = doc.GetElement(materials.ElementAt(0));
+                var yield = mat.get_Parameter(BuiltInParameter.PHY_MATERIAL_PARAM_MINIMUM_YIELD_STRESS);
+                var temp = yield.AsValueString();
+                var youngs = mat.get_Parameter(BuiltInParameter.PHY_MATERIAL_PARAM_YOUNG_MOD);
+                temp = youngs.AsValueString();*/
+
+                var structID = e.get_Parameter(BuiltInParameter.STRUCTURAL_MATERIAL_PARAM);
+                var material = doc.GetElement(structID.AsElementId()) as Material;
+                var properties = doc.GetElement(material.StructuralAssetId);
+
+                var yield = properties.get_Parameter(BuiltInParameter.PHY_MATERIAL_PARAM_MINIMUM_YIELD_STRESS).AsValueString();              
+                
+
+            }
+
+            return Result.Succeeded;
+        }
+    }
+}
